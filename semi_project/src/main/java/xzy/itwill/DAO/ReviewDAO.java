897be5c9e53b509cd9d8d7a9f8110fa16779653d
itwill <itwill@DESKTOP-OP3B1FN>
2024-02-07 18:20:47 +0900

    마이페이지 기능구현중_ 김민준

---
+++ semi_project/src/main/java/xzy/itwill/DAO/ReviewDAO.java
@@ -0,0 +1,307 @@
+package xzy.itwill.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import xyz.itwill.DTO.QaDTO;
+import xyz.itwill.DTO.ReviewDTO;
+
+
+public class ReviewDAO extends JdbcDAO {
+	private static ReviewDAO _dao;
+	
+	public ReviewDAO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	static {
+		_dao=new ReviewDAO();
+	}
+	
+	public static ReviewDAO getDAO() {
+		return _dao;
+	}
+	
+	
+	
+	//검색정보(검색대상과 검색단어)를 전달받아 REVIEW_TABLE에 저장된 게시글 중 검색대상의 
+	//컬럼에 검색단어가 포함된 게시글의 갯수를 검색하여 반환하는 메소드
+	// => 검색 기능을 사용하지 않을 경우 REVIEW_TABLE에 저장된 모든 게시글의 갯수를 검색하여 반환
+	public int selectTotalReview(String search, String keyword) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		int totalCount=0;
+		try {
+			con=getConnection();
+			
+			if(keyword=="") {
+				String sql="select count(*) from review_table";
+				pstmt=con.prepareStatement(sql);
+				
+			} else {
+				String sql="select count(*) from review_table";
+				pstmt=con.prepareStatement(sql);
+			} else {
+				String sql="select count(*) from review_table where "+search+" like '%'||?||'%'";
+				pstmt=con.prepareStatement(sql);
+				pstmt.setString(1, keyword);
+			}
+			
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+				totalCount=rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			System.out.println("[에러]selectTotalReview() 메소드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return totalCount;
+	}
+	
+	
+	// 페이징 처리 관련 정보(시작 행번호와 종료 행번호)와 게시글 검색 기능 관련 정보(검색대상과
+	// 검색단어)를 전달받아 REVIEW_TANLE에 저장된 행을 검색하여 게시글 목록을 반환하는 메소드
+	public List<ReviewDTO> selectReviewList(int startRow, int endRow, String search, String keyword) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<ReviewDTO> reviewList = new ArrayList<ReviewDTO>();
+		try {
+			con = getConnection();
+
+			if (keyword.equals("")) {// 검색 기능을 사용하지 않은 경우
+				String sql = "select * from (select rownum rn, temp.* from (select review_num"
+						+ ", review_member_num, client_name, review_subject, review_content, review_image"
+						+ ", review_register, review_update, review_readcount, review_replay"
+						+ ", from review_table join client_table"
+						+ " on review_member_num=client_num order by review_register desc) temp)"
+						+ " where rn between ? and ?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, startRow);
+				pstmt.setInt(2, endRow);
+			} else {// 검색 기능을 사용한 경우
+				String sql = "select * from (select rownum rn, temp.* from (select review_num"
+						+ ", review_member_num, client_name, review_subject, review_content, review_image"
+						+ ", review_register, review_update, review_readcount, review_replay"
+						+ ", from review_table join client_table"
+						+ " on review_member_num=client_num where " + search + " like '%'||?||'%'"
+						+ " order by review_register desc) temp)"
+						+ " where rn between ? and ?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, keyword);
+				pstmt.setInt(2, startRow);
+				pstmt.setInt(3, endRow);
+			}
+			
+			//여기 밑으로 확인하기
+			
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				ReviewDTO reviewTable = new ReviewDTO();
+				reviewTable.setReviewNum(rs.getInt("review_num"));
+				reviewTable.setReviewMemberNum(rs.getInt("review_member_num"));
+				reviewTable.setReviewName(rs.getString("review_name"));
+				reviewTable.setReviewSubject(rs.getString("review_subject"));
+				reviewTable.setReviewContent(rs.getString("review_content"));
+				reviewTable.setReviewImage(rs.getString("review_image"));
+				reviewTable.setReviewRegister(rs.getString("review_register"));
+				reviewTable.setReviewUpdate(rs.getString("review_update"));
+				reviewTable.setReviewReadcount(rs.getInt("review_readcount"));
+				reviewTable.setReviewReplay(rs.getInt("review_replay"));
+
+				reviewList.add(reviewTable);
+			}
+		} catch (SQLException e) {
+			System.out.println("[에러]selectReviewList() 메소드의 SQL 오류 = " + e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return reviewList;
+	}
+	
+	// REVIEW_SEQ 시퀸스의 다음값(정수값)을 검색하여 반환하는 메소드
+	public int selectReivewNextNum() {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int nextNum = 0;
+		try {
+			con = getConnection();
+
+			String sql = "select review_table_seq.nextval from dual";
+			pstmt = con.prepareStatement(sql);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				nextNum = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			System.out.println("[에러]selectReivewNextNum() 메소드의 SQL 오류 = " + e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return nextNum;
+	}
+	
+	
+	//게시글을 전달받아 REVIEW_TABLE에 행으로 삽입하고 삽입행의 갯수를 반환하는 메소드
+		public int insertReview(ReviewDTO review) {
+			Connection con=null;
+			PreparedStatement pstmt=null;
+			int rows=0;
+			try {
+				con=getConnection();
+				
+				String sql="insert into review_table values(?,?,?,?,?,sysdate,null,0,?)";
+				pstmt=con.prepareStatement(sql);
+				pstmt.setInt(1, review.getReviewNum());
+				pstmt.setInt(2, review.getReviewMemberNum());
+				pstmt.setString(3, review.getReviewSubject());
+				pstmt.setString(4, review.getReviewContent());
+				pstmt.setString(5, review.getReviewImage());
+				pstmt.setInt(6, review.getReviewReplay());
+				
+				rows=pstmt.executeUpdate();
+			} catch (SQLException e) {
+				System.out.println("[에러]insertReview() 메소드의 SQL 오류 = "+e.getMessage());
+			} finally {
+				close(con, pstmt);
+			}
+			return rows;
+		}
+		
+		
+		
+		
+		
+		
+		
+		//글번호를 전달받아 REVIEW 테이블의 단일행을 검색하여 게시글(ReviewDTO 객체)을 반환하는 메소드
+		public QaDTO selectQaByNum(int qaNum) {
+			Connection con=null;
+			PreparedStatement pstmt=null;
+			ResultSet rs=null;
+			QaDTO qa=null;
+			try {
+				con=getConnection();
+				
+				String sql="select qa_num,qa_member,name qa_name,qa_subject,qa_content,qa_image"
+						+ ",qa_register,qa_update,qa_readcount,qa_replay from qa_table join"
+						+ " client_table on qa_table.qa_member=client_table.client_num"
+						+ " where qa_num=?";
+				pstmt=con.prepareStatement(sql);
+				pstmt.setInt(1, qaNum);
+				
+				rs=pstmt.executeQuery();
+				
+				if(rs.next()) {
+					qa=new QaDTO();
+					qa.setQaNum(rs.getInt("qa_num"));
+					qa.setQaMember(rs.getInt("qa_member"));
+					qa.setQaName(rs.getString("qa_name"));
+					qa.setQaSubject(rs.getString("qa_subject"));
+					qa.setQaContent(rs.getString("qa_content"));
+					qa.setQaImage(rs.getString("qa_image"));
+					qa.setQaRegister(rs.getString("qa_register"));
+					qa.setQaUpdate(rs.getString("qa_update"));
+					qa.setQaReadCount(rs.getInt("qa_readCount"));
+					qa.setQaReplay(rs.getInt("qa_replay"));
+				}
+			} catch (SQLException e) {
+				System.out.println("[에러]selectQaByNum() 메소드의 SQL 오류 = "+e.getMessage());
+			} finally {
+				close(con, pstmt, rs);
+			}
+			return qa;
+		}
+		
+		//글번호를 전달받아 REVIEW 테이블의 저장된 행의 게시글 조회수가 1 증가되도록 변경하고 
+		//변경행의 갯수를 반환하는 메소드
+		public int updateQaReadCount(int QaNum) {
+			Connection con=null;
+			PreparedStatement pstmt=null;
+			int rows=0;
+			try {
+				con=getConnection();
+				
+				String sql="update qa_table set qa_readcount=qa_readcount+1 where qa_num=?";
+				pstmt=con.prepareStatement(sql);
+				pstmt.setInt(1, QaNum);
+				
+				rows=pstmt.executeUpdate();
+			} catch (SQLException e) {
+				System.out.println("[에러]updateQaReadCount() 메소드의 SQL 오류 = "+e.getMessage());
+			} finally {
+				close(con, pstmt);
+			}
+			return rows;
+		}
+		
+		//게시글을 전달받아 REVIEW 테이블의 저장된 행의 컬럼값을 변경하고 변경행의 갯수를 반환하는 메소드
+		public int deleteQa(int qaNum) {
+			Connection con=null;
+			PreparedStatement pstmt=null;
+			int rows=0;
+			try {
+				con=getConnection();
+				
+				String sql="delete from qa_table where qa_num=?";
+				pstmt=con.prepareStatement(sql);
+				pstmt.setInt(1, qaNum); 
+			} catch (SQLException e) {
+				System.out.println("[에러]updatQA() 메소드의 SQL 오류 = "+e.getMessage());
+			} finally {
+				close(con, pstmt);
+			}
+			return rows;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- semi_project/src/main/webapp/my_page/review_list.jsp
+++ semi_project/src/main/webapp/my_page/review_list.jsp
@@ -1,7 +1,81 @@
+<%@page import="java.util.Date"%>
+<%@page import="java.text.SimpleDateFormat"%>
+<%@page import="java.util.List"%>
+<%@page import="xyz.itwill.DTO.ReviewDTO"%>
+<%@page import="xzy.itwill.DAO.ReviewDAO"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+<%
+	//게시글 검색 기능에 필요한 전달값(검색대상과 검색단어)을 반환받아 저장
+	String search=request.getParameter("search");//검색대상
+	if(search==null) {//전달값이 없는 경우 - 게시글 검색 기능을 사용하지 않은 경우
+		search="";
+	}
+	
+	String keyword=request.getParameter("keyword");//검색단어
+	if(keyword==null) {//전달값이 없는 경우
+		keyword="";
+	}
+
+	//페이징 처리에 필요한 전달값(페이지번호과 게시글갯수)을 반환받아 저장
+	int pageNum=1;//페이지번호- 전달값이 없는 경우 저장된 초기값 설정
+	if(request.getParameter("pageNum")!=null) {//전달값이 있는 경우
+		pageNum=Integer.parseInt(request.getParameter("pageNum"));
+	}
+	
+	int pageSize=10;//게시글갯수- 전달값이 없는 경우 저장된 초기값 설정
+	if(request.getParameter("pageSize")!=null) {//전달값이 있는 경우
+		pageSize=Integer.parseInt(request.getParameter("pageSize"));
+	}
+
+	//검색정보(검색대상과 검색단어)를 전달받아 REVIEW 테이블에 저장된 게시글 중 검색대상의 컬럼에
+	//검색단어가 포함된 게시글의 갯수를 검색하여 반환하는 ReviewDAO 클래스의 메서드 호출
+	// => 검색 기능을 사용하지 않을 경우 REVIEW 테이블에 저장된 모든 게시글의 갯수를 반환
+	int totalReview=ReviewDAO.getDAO().selectTotalReview(search, keyword);//검색된 게시글의 총갯수
+	
+	//전체 페이지의 총갯수를 계산하여 저장
+	//int totalPage=totalReview/pageSize+totalReview%pageSize==0?0:1;
+	int totalPage=(int)Math.ceil((double)totalReview/pageSize);//페이지의 총갯수
+ 
+	//전달받은 페이지번호가 비정상적인 경우
+	if(pageNum<=0 || pageNum>totalPage) {
+		pageNum=1;
+	}
+	
+	//페이지번호에 대한 게시글의 시작 행번호를 계산하여 저장
+	//ex) 1Page : 1, 2Page : 11, 3Page : 21, 4Page : 31, ...
+	int startRow=(pageNum-1)*pageSize+1;
+	
+	//페이지번호에 대한 게시글의 종료 행번호를 계산하여 저장
+	//ex) 1Page : 10, 2Page : 20, 3Page : 30, 4Page : 40, ...
+	int endRow=pageNum*pageSize;
+	
+	//마지막 페이지의 게시글의 종료 행번호가 게시글의 총갯수보다 많은 경우 종료 행번호 변경
+	if(endRow>totalReview) {
+		endRow=totalReview;
+	}
+	
+	//페이징 처리 관련 정보(시작 행번호와 종료 행번호)와 게시글 검색 기능 관련 정보(검색대상과
+	//검색단어)를 전달받아 REVIEW 테이블에 저장된 행을 검색하여 게시글 목록을 반환하는 ReviewDAO 
+	//클래스의 메소드 호출
+	List<ReviewDTO> reviewList=ReviewDAO.getDAO().selectReviewList(startRow, endRow, search, keyword);
+	
+	//session 객체에 저장된 권한 관련 속성값을 반환받아 저장
+	// => 로그인 상태의 사용자에게만 글쓰기 권한 제공
+	// => 게시글이 비밀글인 경우 로그인 상태의 사용자가 게시글 작성자이거나 관리자인 경우에만 권한 제공
+	//MemberDTO loginMember=(MemberDTO)session.getAttribute("loginMember");
+	
+	//서버 시스템의 현재 날짜를 제공받아 저장
+	// => 게시글 작성날짜와 비교하여 게시글 작성날짜를 다르게 출력되도록 응답 처리
+	String currentDate=new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+	
+	//페이지에 출력될 게시글의 일련번호 시작값을 계산하여 저장
+	// => 검색된 게시글의 총갯수가 91개인 경우 >> 1Page : 91, 2Page : 81, 3Page, 71
+	int displayNum=totalReview-(pageNum-1)*pageSize;
+%>
+
 <%-- 네비게이션 바 --%> 
 <style type="text/css">
 body {
@@ -187,6 +261,7 @@ a {
 		</div>
 		<div class="col col-lg-10">
 			<h1 class="subTitle1">내가 작성한 리뷰</h1>
+			
 			<div class="tabType">
 				<ul class="item2">
 					<li class="active"><a href="<%=request.getContextPath()%>/main_page/main.jsp?group=my_page&worker=review"><span>작성 가능한 리뷰</span></a></li>
@@ -194,30 +269,115 @@ a {
 				</ul>
 			</div>
 					
+			<%-- 검색된 게시글 총갯수 출력 --%>		
+			<div id="review_title">리뷰 목록(<%=totalReview %>)</div>
+			<div style="text-align: right;">
+				게시글갯수 : 
+				<select id="reviewCount">
+					<option value="10" <%  { %> selected <% } %>>&nbsp;10개&nbsp;</option>	
+					<option value="20" <%  { %> selected <% } %>>&nbsp;20개&nbsp;</option>	
+					<option value="50" <%  { %> selected <% } %>>&nbsp;50개&nbsp;</option>	
+					<option value="100" <% { %> selected <% } %>>&nbsp;100개&nbsp;</option>	
+				</select>
+				&nbsp;&nbsp;&nbsp;
+			</div>
+				
+			<div class="mbOutTop">	
+				<div class="tableType tb_review" id="reviewArea"></div>
+				
+				
+				<table>
+					<tr>
+						<th width="100">글번호</th>
+						<th width="500">제목</th>
+						<th width="100">작성자</th>
+						<th width="100">조회수</th>
+						<th width="200">작성일</th>
+					</tr>
+		
+				<%-- 연습 --%>
+				<% if(totalReview==0) {//검색된 게시글이 없는 경우 %>
+					<tr>
+						<td colspan="5">검색된 게시글이 없습니다.</td>
+					</tr>
+				<% } else {//검색된 게시글이 있는 경우 %>
+					<% for(ReviewDTO reviewTable : reviewList) { %>	
+						<tr>
+						<%-- 게시글의 글번호가 아닌 게시글의 일련번호 출력 --%>
+						<td><%=displayNum %></td>
+						<% displayNum--; %><%-- 게시글 일련번호를 1씩 감소하여 저장 --%>
+				
+						<%-- 제목 출력 --%>
+						<td class="subject">
+							<%-- 게시글이 답글인 경우에 대한 응답 처리 --%>
+							<% if(reviewTable.getReviewRestep() != 0) {//답글인 경우 %>
+							<%-- 게시글(답글)의 깊이를 제공받아 왼쪽 여백 설정 --%>
+							<span style="margin-left: <%=review.getReviewRelevel()*20%>px;">┗[답글]</span>
+						<% } %>
 					
-										<div id="review_title">리뷰 목록</div>
-
-	<div style="text-align: right;">
-		게시글갯수 : 
-		<select id="reviewCount">
-			<option value="10" <%  { %> selected <% } %>>&nbsp;10개&nbsp;</option>	
-			<option value="20" <%  { %> selected <% } %>>&nbsp;20개&nbsp;</option>	
-			<option value="50" <%  { %> selected <% } %>>&nbsp;50개&nbsp;</option>	
-			<option value="100" <% { %> selected <% } %>>&nbsp;100개&nbsp;</option>	
-		</select>
-		&nbsp;&nbsp;&nbsp;
-		
-		
-		
-		
-	<div class="mbOutTop">	
-	<div class="tableType tb_review" id="reviewArea">
 					
 					
-					
-		</div>
+					<%-- 게시글 상태를 비교하여 제목과 링크를 구분해 응답 처리 --%>
+					<%
+						String url=request.getContextPath()+"/index.jsp?group=review&worker=review_detail"
+							+"&reviewNum="+reviewTable.getReviewNum()+"&pageNum="+pageNum+"&pageSize="+pageSize
+							+"&search="+search+"&keyword="+keyword;
+					%>
+						<%-- 로그인 상태의 사용자가 게시글 작성자인 경우 또는 로그인 상태의 
+						사용자가 관리자인 경우 제목과 링크 제공 --%>
+						<%-- <% if(loginMember!=null && (loginMember.getMemberNum()==reviewTable.getReviewMemberNum() 
+							|| loginMember.getMemberStatus()==9)) { %>
+							<a href="<%=url%>"><%=reviewTable.getReviewSubject() %></a>
+						<% } else { %>
+							게시글 작성자 또는 관리자만 확인 가능합니다.						
+						<% } %>	--%>
+					<%--	
+					<% } if(reviewTable.getReviewStatus()==0) {//삭제 게시글인 경우 %>
+						<span class="subject_hidden">삭제글</span>
+						게시글 작성자 또는 관리자에 의해 삭제된 게시글입니다.	
+					<% } %>
+				</td>
+				
+				<% if(reviewTable.getReviewStatus()!=0) {//삭제 게시글이 아닌 경우 %>
+				--%>				
+					<%-- 작성자(회원이름) 출력 --%>
+					<td><%=reviewTable.getReviewName() %></td>
+								
+					<%-- 조회수 출력 --%>
+					<td><%=reviewTable.getReviewReadcount() %></td>
+								
+					<%-- 작성일 출력 : 오늘 작성된 게시글인 경우 시간만 출력하고 오늘 작성된
+					게시글이 아닌 경우 날짜와 시간 출력 --%>	
+					<td>
+						<%-- 오늘 작성된 게시글인 경우 --%>
+						<% if(currentDate.equals(reviewTable.getReviewRegister().substring(0, 10))) { %>
+							<%=reviewTable.getReviewRegister().substring(11) %>
+						<% } else { %>
+							<%=reviewTable.getReviewRegister() %>
+						<% } %>
+				
+				<%--	</td>		
+				<% } else {//삭제글인 경우 %>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				<% } %>
+				--%>
+			</tr>
+			<% } %>
+			<% } %>
+			</table>
 		
-<li>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			<li>
 					<!-- 상단 프로필과 제품 -->
 					<div class="summary">
 						<!-- 프로필 -->						
